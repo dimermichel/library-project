dfea378c0c4f150032d672ae70b945d8916bbe54
Michel Maia <dimermichel@gmail.com>
2020-02-03 10:23:08 -0500

    App Express various enhancements

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,15 @@ router.post('/reviews/add', (req, res, next) => {
     });
 });
 
+router.post('/book/:id/delete', (req, res, next) => {
+
+  Book.findByIdAndRemove(req.params.id)
+    .then(book => {
+      res.redirect('/books');
+    })
+    .catch(err => console.log(err));
+});
+
 router.get('/book/:id', (req, res, next) => {
   const bookId = req.params.id;
   if (!/^[0-9a-fA-F]{24}$/.test(bookId)) {
@@ -58,8 +67,17 @@ router.post('/authors/add', (req, res, next) => {
 
 router.get('/books/edit', (req, res, next) => {
   Book.findOne({_id: req.query.book_id})
+  .populate('author')
   .then((book) => {
-    res.render("book-edit", {book});
+
+    Author.find()
+      .then( availableAuthors => {
+        const newAvailableAuthors =  availableAuthors.filter(
+          oneAuthor => !oneAuthor._id.equals(book.author[0]._id)
+        )
+        res.render("book-edit", {book, newAvailableAuthors})
+      })
+      .catch(error => console.log(error))
   })
   .catch((error) => {
     console.log(error);
@@ -78,7 +96,13 @@ router.post('/books/edit', (req, res, next) => {
 });
 
 router.get('/books/add', (req, res, next) => {
-  res.render("book-add");
+  Author.find()
+  .then(availableAuthors => {
+    console.log(availableAuthors)
+    res.render("book-add", {availableAuthors});
+
+  })
+  .catch(err => console.log(err))
 });
 
 router.post('/books/add', (req, res, next) => {
@@ -86,7 +110,8 @@ router.post('/books/add', (req, res, next) => {
   const newBook = new Book({ title, author, description, rating})
   newBook.save()
   .then((book) => {
-    res.redirect('/books');
+    console.log(book)
+    res.redirect('/books')
   })
   .catch((error) => {
     console.log(error);
@@ -103,6 +128,15 @@ router.get('/books', (req, res, next) => {
       console.log('Error while getting the books from the DB: ', error);
     })
 });
+
+router.get('/authors', (req, res) => {
+  Author.find()
+  .then(authorsFromDb => {
+    console.log(`Authors form DB: ${authorsFromDb}`)
+    res.render('authors', {authors: authorsFromDb})
+  })
+  .catch(err => console.log(err))
+})
 
 /* GET home page */
 router.get('/', (req, res, next) => {
